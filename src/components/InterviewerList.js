f94979fd099df9398ee607e2977aa566703496b8
Jeandre <jeandrev1414@gmail.com>
2022-08-10 15:20:24 -0600

    Refactor to define function in InterviewerList and pass id in InterviewerListItem

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -12,11 +12,10 @@ const InterviewerList = (props) => {
 
           <InterviewerListItem
             key={interviewer.id}
-            id={interviewer.id}
             name={interviewer.name}
             avatar={interviewer.avatar}
             selected={props.interviewer === interviewer.id}
-            setInterviewer={props.setInterviewer}
+            setInterviewer={() => props.setInterviewer(interviewer.id)}
           />
 
         )}
