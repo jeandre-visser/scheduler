8e9a49ddf7db4ff2d40de5d74efdf7a21ed0a28a
Jeandre <jeandrev1414@gmail.com>
2022-08-10 16:49:55 -0600

    Update with return

--- src/components/Appointment/Empty.js
+++ src/components/Appointment/Empty.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 const Empty = (props) => {
+  return (
   <main className="appointment__add">
     <img
       className="appointment__add-button"
@@ -9,6 +10,7 @@ const Empty = (props) => {
       onClick={props.onAdd}
     />
   </main>
+  );
 }
 
 export default Empty;
