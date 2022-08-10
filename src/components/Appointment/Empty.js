110a1f27b40ff099095724d3fd0a5d30fe71057b
Jeandre <jeandrev1414@gmail.com>
2022-08-10 16:42:40 -0600

    Add Empty component

---
+++ src/components/Appointment/Empty.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Empty = (props) => {
+  <main className="appointment__add">
+    <img
+      className="appointment__add-button"
+      src="images/add.png"
+      alt="Add"
+    />
+  </main>
+}
+
+export default Empty;
