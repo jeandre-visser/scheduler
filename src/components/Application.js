a100887fded7924721b62ad2e871e96f7d4a4a3c
Jeandre <jeandrev1414@gmail.com>
2022-08-11 16:54:00 -0600

    Implement schedule function

--- src/components/Application.js
+++ src/components/Application.js
@@ -32,6 +32,19 @@ export default function Application(props) {
 
   const dailyAppointments = getAppointmentsForDay(state, state.day);
 
+  const schedule = dailyAppointments.map((appointment) => {
+    const interview = getInterview(state, appointment.interview);
+
+    return (
+      <Appointment
+        key={appointment.id}
+        id={appointment.id}
+        time={appointment.time}
+        interview={interview}
+      />
+    );
+  });
+
   return (
     <main className="layout">
       <section className="sidebar">
@@ -55,12 +68,7 @@ export default function Application(props) {
       />
       </section>
       <section className="schedule">
-        {dailyAppointments.map(appointment => 
-          <Appointment 
-            key={appointment.id}
-            {...appointment}
-          />  
-        )}
+        {schedule}
         <Appointment key="last" time="5pm" />
       </section>
     </main>
