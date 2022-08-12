d354879acb8936277605f7f98844fa30bd90e084
Jeandre <jeandrev1414@gmail.com>
2022-08-11 20:59:05 -0600

    Import useVisualMode and add mode constants

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,10 +3,13 @@ import "./styles.scss"
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
-
+import useVisualMode from "hooks/useVisualMode";
 
 const Appointment = (props) => {
 
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  
   return (
     <article className="appointment">
 
