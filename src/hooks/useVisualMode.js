da0bad53cff07757d801efbc334425707ba879e1
Jeandre <jeandrev1414@gmail.com>
2022-08-11 20:54:48 -0600

    Pull setHistory out of the if statement

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -3,18 +3,22 @@ import React, { useState } from "react";
 const useVisualMode = (initial) => {
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
-  const transition = (newMode, replace = false) => { 
 
-    if (replace) {
-      setHistory(history => {
+
+  const transition = (newMode, replace = false) => { 
+    setMode(newMode)
+    setHistory(history => {
+
+      if(replace) {
         const newHistory = [...history];
         newHistory.splice(-1, 1, newMode);
         return newHistory;
-      })
-    } else {
-      setHistory(history => [...history, newMode]);
-    }
-  };
+
+      } else {
+        return [...history, newMode];
+      }
+    }) 
+  }
   
   const back = () => { 
     setHistory(history => {
