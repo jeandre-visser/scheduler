175ee8ecd035d32a90ac60ddd5a72acc4b34e924
Jeandre <jeandrev1414@gmail.com>
2022-08-12 14:44:21 -0600

    Import getInterviewersForDay selector, create dailyInterviewers array, and pass into Appointment component

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,7 +4,7 @@ import React, { useState, useEffect } from "react";
 import "components/Appointment";
 import Appointment from "components/Appointment";
 import axios from "axios";
-import { getAppointmentsForDay, getInterview } from "helpers/selectors";
+import { getAppointmentsForDay, getInterview, getInterviewersForDay } from "helpers/selectors";
 
 
 export default function Application(props) {
@@ -32,6 +32,8 @@ export default function Application(props) {
 
   const dailyAppointments = getAppointmentsForDay(state, state.day);
 
+  const dailyInterviewers = getInterviewersForDay(state, state.day);
+
   const schedule = dailyAppointments.map((appointment) => {
     const interview = getInterview(state, appointment.interview);
 
@@ -41,6 +43,7 @@ export default function Application(props) {
         id={appointment.id}
         time={appointment.time}
         interview={interview}
+        interviewers={dailyInterviewers}
       />
     );
   });
