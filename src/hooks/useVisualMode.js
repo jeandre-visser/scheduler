eee6e84e4373ca522e5fb94384dc9d67361551cf
Jeandre <jeandrev1414@gmail.com>
2022-08-11 19:49:32 -0600

    Add useVisualMode hook

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+
+const useVisualMode = (initial) => {
+  const [mode, setMode] = useState(initial)
+  
+  
+  return (
+    mode
+
+  );
+}
+
+export default useVisualMode;
