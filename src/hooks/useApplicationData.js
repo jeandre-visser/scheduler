b33114190a6e669e5bd89ffafa39a93693728b12
Jeandre <jeandrev1414@gmail.com>
2022-08-15 19:20:34 -0600

    Create useApplicationData custom hook to handle all the initial data from the API, and updating the state

---
+++ src/hooks/useApplicationData.js
@@ -0,0 +1,74 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const useApplicationData = () => {
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {},
+    interviewers: {}
+  });
+  
+  const setDay = day => {
+    setState({...state, day })
+  };
+
+  useEffect(() => {
+    Promise.all([
+      axios.get('/api/days'),
+      axios.get('/api/appointments'),
+      axios.get('/api/interviewers')
+    ]).then((all) => {
+      setState(state => ({...state, days: all[0].data, appointments: all[1].data, interviewers: all[2].data}))
+    })
+  }, [])
+
+
+  const bookInterview = (id, interview) => {
+    const appointment = {
+      ...state.appointments[id],
+      interview: { ...interview }
+    };
+
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment
+    };
+
+    return axios
+    .put(`/api/appointments/${id}`, {interview})
+    .then(res => setState(state => ({
+      ...state,
+      appointments
+    })));
+
+  };
+  
+  const cancelInterview = (id) => {
+    const appointment = {
+      ...state.appointments[id],
+      interview: null
+    };
+
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment
+    };
+
+    return axios
+      .delete(`/api/appointments/${id}`)
+      .then(res => setState(state => ({
+        ...state,
+        appointments
+      })));
+  };
+
+  return {
+    state, 
+    setDay,
+    bookInterview,
+    cancelInterview
+  }
+};
+
+export default useApplicationData;
