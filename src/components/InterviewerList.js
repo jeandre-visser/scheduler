72a377703ae61ef707e94960c20befc140898b1c
Jeandre <jeandrev1414@gmail.com>
2022-08-16 15:30:08 -0600

    Import prop.types to ensure interviewers prop is an Array and is required

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "components/InterviewerList.scss";
 import InterviewerListItem from "./InterviewerListItem";
+import PropTypes from 'prop-types';
 
 const InterviewerList = (props) => {
 
@@ -23,6 +24,10 @@ const InterviewerList = (props) => {
       </ul>
     </section>
   );
+};
+
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired
 };
 
 export default InterviewerList;
