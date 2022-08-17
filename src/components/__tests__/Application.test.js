a05ae2d78704da3b41ca5cc4114398eb92011382
Jeandre <jeandrev1414@gmail.com>
2022-08-16 21:52:06 -0600

    Add tests for editing an interview, also add comments for deleting interview test

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -41,7 +41,7 @@ describe("Application", () => {
     // Click the "Save" button on that same appointment.
     fireEvent.click(getByText(appointment, "Save"));
 
-    // Verify that the the appointment element contains the text "SAVING" immediately after the "Save" button is clicked
+    // Verify that the the appointment element contains the text "SAVING" immediately after Deletethe "Save" button is clicked
 
     expect(getByText(appointment, "SAVING")).toBeInTheDocument();
     
@@ -81,12 +81,49 @@ describe("Application", () => {
     // Wait until the element with the "Add" button is displayed.
     await waitForElement(() => getByAltText(appointment, "Add"));
 
-    // Check that the DayListItem with the text "Monday" also has the text "2 spots remaining".
+    // Check that the DayListItem with the text "Monday" also has the text "1 spot remaining".
+    /**
+     * Changed to "1 spot remaining" instead of "2 spots remaining" like in compass because the previous test ("loads data, books an interview and reduces the spots remaining for the first day by 1") reduces the amount of spots remaining by 1, while this test adds an additional spot making there only be 1 spot remaining instead of 2
+     * Another option is to keep the 2 spots remaining but then skip the previous test and run it separately
+     */
     const day = getAllByTestId(container, "day").find(day =>
       queryByText(day, "Monday")
     );
 
-    expect(getByText(day, "2 spots remaining")).toBeInTheDocument();
+    expect(getByText(day, "1 spot remaining")).toBeInTheDocument();
+  })
+
+  it("loads data, edits an interview and keeps the spots remaining for Monday the same", async () => {
+    // Render the Application
+    const { container } = render(<Application />);
+
+    // Wait until the text "Archie Cohen" is displayed
+    await waitForElement(() => getByText(container, "Archie Cohen"));
+    const appointment = getAllByTestId(container, "appointment").find(appointment => 
+      queryByText(appointment, "Archie Cohen")
+    );
+
+    // Click the "Edit" button on the first booked appointment.
+    fireEvent.click(getByAltText(appointment, "Edit"));
+
+    // Enter a new name into the input with the placeholder "Enter Student Name" 
+    fireEvent.change(getByPlaceholderText(appointment, "Enter Student Name"), {
+      target: { value: "Donald Trump" }
+    });
+
+    // Click on the first interviewer in the list
+    fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
+
+    // Click save
+    fireEvent.click(getByText(appointment, "Save"));
+
+    // Check that the element with the text "SAVING" is displayed.
+    expect(getByText(appointment, ("SAVING"))).toBeInTheDocument();
+
+    // Wait until the element shows the new student name and Sylvia Palmer as the interviewer
+    await waitForElement(() => getByText(appointment, "Donald Trump"));
+
+    expect(getByText(appointment, "Sylvia Palmer")).toBeInTheDocument();
   })
 
 })
