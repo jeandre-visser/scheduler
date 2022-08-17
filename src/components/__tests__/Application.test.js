f35ca529af3c72cde390c26bf0893d840b3075c8
Jeandre <jeandrev1414@gmail.com>
2022-08-16 20:34:40 -0600

    Find specific day node and check that the day also has no spots remaining

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { render, cleanup, waitForElement, fireEvent, getByText, prettyDOM, getAllByTestId, getByAltText, getByPlaceholderText } from "@testing-library/react";
+import { render, cleanup, waitForElement, fireEvent, getByText, prettyDOM, getAllByTestId, getByAltText, getByPlaceholderText, queryByText } from "@testing-library/react";
 
 import Application from "components/Application";
 
@@ -34,23 +34,35 @@ describe("Application", () => {
     fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
       target: { value: "Lydia Miller-Jones" }
     });
+
+    // Click the first interviewer in the list.
     fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
 
+    // Click the "Save" button on that same appointment.
     fireEvent.click(getByText(appointment, "Save"));
 
     // Verify that the the appointment element contains the text "SAVING" immediately after the "Save" button is clicked
+
     expect(getByText(appointment, "SAVING")).toBeInTheDocument();
     
     // Enter the name "Lydia Miller-Jones" into the input with the placeholder "Enter Student Name".
+    // Wait until the element with the text "Lydia Miller-Jones" is displayed.
     await waitForElement(() => getByText(appointment, "Lydia Miller-Jones"));
+
+    // Find the specific day node that contains the text "Monday"
+    const day = getAllByTestId(container, "day").find(day =>
+      queryByText(day, "Monday")
+    );
+    // Check that the day with the text "Monday" also has the text "no spots remaining".
+    expect(getByText(day, /no spots remaining/i)).toBeInTheDocument();
   })
 })
 
 
 
 
-// Click the first interviewer in the list.
-// Click the "Save" button on that same appointment.
-// Check that the element with the text "Saving" is displayed.
-// Wait until the element with the text "Lydia Miller-Jones" is displayed.
+
+
+
+
 // Check that the DayListItem with the text "Monday" also has the text "no spots remaining".
