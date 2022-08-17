977d96931415945aad32291655b266243cf6c31f
Jeandre <jeandrev1414@gmail.com>
2022-08-17 16:45:37 -0600

    Add test for editing an interview

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -24,4 +24,26 @@ describe("Appointments", () => {
     cy.contains(".appointment__card--show", "Sylvia Palmer");
 
   })
+
+  it("should edit an interview", () => {
+    // click edit button
+    cy.get("[alt=Edit]")
+      .first()
+      .click({force: true});
+
+    // type in student name
+    cy.get("[data-testid=student-name-input]")
+    .clear()
+    .type("Lydia Miller-Jones");
+
+    // select interviewer
+    cy.get("[alt='Tori Malcolm']").click();
+
+    // click save
+    cy.contains("Save").click();
+
+    // verify that the student and interviewer names are shown
+    cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+    cy.contains(".appointment__card--show", "Tori Malcolm");
+  })
 });
