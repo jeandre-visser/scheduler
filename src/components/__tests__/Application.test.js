cdbb5d7e59bbdc35ffd41acca579c897dc96292e
Jeandre <jeandrev1414@gmail.com>
2022-08-16 20:18:03 -0600

    verify that the the appointment element contains the text Saving immediately after the Save button is clicked

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,17 +1,54 @@
 import React from "react";
 
-import { render, cleanup, waitForElement, fireEvent } from "@testing-library/react";
+import { render, cleanup, waitForElement, fireEvent, getByText, prettyDOM, getAllByTestId, getByAltText, getByPlaceholderText } from "@testing-library/react";
 
 import Application from "components/Application";
 
 afterEach(cleanup);
 
-it("defaults to Monday and changes the schedule when a new day is selected", async () => {
-  const { getByText } = render(<Application />);
-
-  await waitForElement(() => getByText("Monday"));
-
-  fireEvent.click(getByText("Tuesday"));
+describe("Application", () => {
+  it("defaults to Monday and changes the schedule when a new day is selected", async () => {
+    const { getByText } = render(<Application />);
   
-  expect(getByText("Leopold Silvers")).toBeInTheDocument();
-});
+    await waitForElement(() => getByText("Monday"));
+  
+    fireEvent.click(getByText("Tuesday"));
+  
+    expect(getByText("Leopold Silvers")).toBeInTheDocument();
+  });
+
+  it("loads data, books an interview and reduces the spots remaining for the first day by 1", async () => {
+    // Render the Application
+    const { container, debug } = render(<Application/>)
+    
+    // Wait until the text "Archie Cohen" is displayed
+    await waitForElement(() => getByText(container, "Archie Cohen"));
+
+    const appointments = getAllByTestId(container, "appointment");
+
+    const appointment = getAllByTestId(container, "appointment")[0];
+
+    // Click the "Add" button on the first empty appointment.
+    fireEvent.click(getByAltText(appointment, "Add"));
+
+    fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
+      target: { value: "Lydia Miller-Jones" }
+    });
+    fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
+
+    fireEvent.click(getByText(appointment, "Save"));
+
+    // Verify that the the appointment element contains the text "SAVING" immediately after the "Save" button is clicked
+    expect(getByText(appointment, "SAVING")).toBeInTheDocument();
+    
+  })
+})
+
+
+
+// Enter the name "Lydia Miller-Jones" into the input with the placeholder "Enter Student Name".
+// Click the first interviewer in the list.
+// Click the "Save" button on that same appointment.
+// Check that the element with the text "Saving" is displayed.
+// Wait until the element with the text "Lydia Miller-Jones" is displayed.
+// Check that the DayListItem with the text "Monday" also has the text "no spots remaining".
