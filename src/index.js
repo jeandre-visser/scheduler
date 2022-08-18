91107d23a86217b110bccdcf881f9cdec4f65db5
Jeandre <jeandrev1414@gmail.com>
2022-08-17 20:53:10 -0600

    Configure default base URL

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import axios from "axios";
 import "index.scss";
+
+if (process.env.REACT_APP_API_BASE_URL) {
+  axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
+}
 
 import Application from "components/Application";
 
 ReactDOM.render(<Application />, document.getElementById("root"));
+
+
